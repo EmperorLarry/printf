e8137bc919f503b6eab6dad6ffac9ddade79b11c
Andrewingit <alexanderobohandrew@gmail.comgit config --global user.email alexanderobohandrew@gmail.com>
2022-10-18 15:12:11 -0700

    corrections on the files

--- funtions1.c
+++ funtions1.c
@@ -1,4 +1,4 @@
-include "main.h"
+#include "main.h"
 /** PRINT UNSIGNED NUMBER ***/
 
 /**
@@ -17,6 +17,7 @@ int print_unsigned(va_list types, char buffer[],
 {
 	int i = BUFF_SIZE - 2;
 	unsigned long int num = va_arg(types, unsigned long int);
+
 	num = convert_size_unsgnd(num, size);
 	if (num == 0)
 		buffer[i--] = '0';
@@ -47,6 +48,7 @@ int print_octal(va_list types, char buffer[],
 	int i = BUFF_SIZE - 2;
 	unsigned long int num = va_arg(types, unsigned long int);
 	unsigned long int init_num = num;
+
 	UNUSED(width);
 	num = convert_size_unsgnd(num, size);
 	if (num == 0)
@@ -124,6 +126,7 @@ int print_hexa(va_list types, char map_to[], char buffer[],
 	int i = BUFF_SIZE - 2;
 	unsigned long int num = va_arg(types, unsigned long int);
 	unsigned long int init_num = num;
+
 	UNUSED(width);
 	num = convert_size_unsgnd(num, size);
 	if (num == 0)
@@ -142,3 +145,4 @@ int print_hexa(va_list types, char map_to[], char buffer[],
 	i++;
 	return (write_unsgnd(0, i, buffer, flags, width, precision, size));
 }
+
